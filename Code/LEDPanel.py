6ee28049b531a55a93714a2415b00f38c1593843
kadeseb <binardgiry.sebastien@gmail.com>
2022-04-01 16:45:35 +0200

    qsd

--- Code/LEDPanel.py
+++ Code/LEDPanel.py
@@ -17,6 +17,7 @@ class Controller:
         self.adapter = pygatt.GATTToolBackend()
         self.adapter.start()
         self.macAddr = macAddr
+        self._connect()
 
     def _connect(self):
         while (True):
@@ -28,9 +29,6 @@ class Controller:
                 print("[LEDPanel] La connexion au bandeau a échoué !")
                 continue
 
-    def _disconnect(self):
-        self.device.disconnect()
-
     def stopAdapter(self):
         self.adapter.stop()
 
@@ -40,22 +38,14 @@ class Controller:
     '''
 
     def powerOff(self):
-        self._connect()
         self.device.char_write_handle(self.HANDLE, self.POWER_OFF_CMD)
 
-        self._disconnect()
     def powerOn(self):
-        self._connect()
         self.setColor(0, 0, 255)
         self.device.char_write_handle(self.HANDLE, self.POWER_ON_CMD)
-        self._disconnect()
 
     def setColor(self, red, green, blue):
-        self._connect()
         self.device.char_write_handle(self.HANDLE, bytearray([0x7E, 0x07, 0x05, 0x03, red, green, blue, 0x10, 0xEF]))
-        self._disconnect()
 
     def setBrightness(self, brightness):
-        self._connect()
         self.device.char_write_handle(self.HANDLE, bytearray([0x7E, 0x04, 0x01, brightness, 0x01, 0xFF, 0xFF, 0x00, 0xEF]))
-        self._disconnect()
